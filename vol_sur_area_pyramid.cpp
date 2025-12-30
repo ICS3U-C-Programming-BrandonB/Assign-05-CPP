04f404971912606fe7ee058288fb00a202ecba81
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-12-30 20:37:00 +0000

    changes

--- vol_sur_area_pyramid.cpp
+++ vol_sur_area_pyramid.cpp
@@ -8,12 +8,13 @@
 #include <iostream>
 #include <string>
 
+// Function to calculate surface area
 float calculateSurfaceArea(float surBase, float surHeight) {
     float surfaceArea = surBase * surBase + 2.0 * surBase *
     std::sqrt((surBase * surBase / 4.0) + surHeight * surHeight);
     return surfaceArea;
 }
-
+// Function to calculate volume
 float calculateVolume(float base, float height) {
     float volume = (base * base) * height / 3.0;
     return volume;
@@ -22,6 +23,7 @@ float calculateVolume(float base, float height) {
 int main() {
     float base, height;
 
+    // Get user input
     try {
         std::cout << "Enter the base of the Square Pyramid: ";
         std::cin >> base;
@@ -30,9 +32,11 @@ int main() {
     } catch (const std::invalid_argument&) {
         std::cout << "Invalid input. Please enter a valid number." << std::endl;
 }
+    // Call functions
     float surface_area = calculateSurfaceArea(base, height);
     float volume = calculateVolume(base, height);
 
+    // Display results
     std::cout << "The surface area of the Square Pyramid is: "
     << std::fixed << std::setprecision(2) << surface_area << '\n';
     std::cout << "The volume of the Square Pyramid is: "
